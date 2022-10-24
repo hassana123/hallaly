045c382bd0b71e1439d15618c16bbf29c8c0e03e
hassana123 <hassanaabdll1@gmail.com>
2022-10-24 17:39:10 +0100

    added two images

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,8 +1,10 @@
 import React from "react";
+import error from "./images/404.jpg";
 import "./styles/errorPage.css";
 const NoMatch = () => {
   return (
-    <div>
+    <div className="noMatch-container">
+      <img src={error} alt="404" />
       <h3>YOU DON DEY CARRY US GO WHERE WE NO KNOW</h3>
     </div>
   );
