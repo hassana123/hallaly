99313bff0148e9651641d6d27f0e13ea3b3b0665
hassana123 <hassanaabdll1@gmail.com>
2022-10-25 22:31:44 +0100

    added other funtionalities

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -2,12 +2,14 @@ import React from "react";
 import Profile from "./Profile";
 import { useState, useEffect } from "react";
 import Pagination from "./Pagination";
+import load from "./images/loading.jpg";
 
 const AllUsers = () => {
   const [loading, setloading] = useState(false);
   const [users, setUsers] = useState([]);
   const [currentUsers, setCurrentUsers] = useState(1);
-  const [userPerPage, setUserPerPage] = useState(12);
+  const [userPerPage] = useState(12);
+  const [disabled, setDisabled] = useState(false);
   useEffect(() => {
     const fetchRandomUser = async () => {
       setloading(true);
@@ -27,10 +29,34 @@ const AllUsers = () => {
   const paginate = (pageNumber) => {
     setCurrentUsers(pageNumber);
   };
+
+  function handleNext(e) {
+    e.preventDefault();
+    if (currentUsers < Math.ceil(users.length / userPerPage)) {
+      setDisabled(false);
+      setCurrentUsers((next) => next + 1);
+    } else {
+      setDisabled(true);
+    }
+  }
+  function handlePrev(e) {
+    e.preventDefault();
+    if (currentUsers > 0) {
+      setDisabled(false);
+      setCurrentUsers((prev) => prev - 1);
+    } else {
+      setDisabled(true);
+    }
+  }
+
+  console.log(disabled);
   return (
     <div>
       {loading ? (
-        "Loading........."
+        <div className="loading">
+          <img src={load} alt="loading" />
+          <p>Efile eje ko COOK</p>
+        </div>
       ) : (
         <>
           <Pagination
@@ -38,6 +64,12 @@ const AllUsers = () => {
             totalUsers={users.length}
             paginate={paginate}
           />
+          <button disabled={disabled} onClick={handleNext}>
+            Next
+          </button>
+          <button disabled={currentUsers <= 1} onClick={handlePrev}>
+            Previous
+          </button>
           <Profile users={currentListUsers} />
         </>
       )}
